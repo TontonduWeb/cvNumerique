441a6867c732d9baa4d0e111c0d313f868d9aee2
TontonduWeb <matthieudejean@hotmail.com>
2024-01-09 14:31:14 +0100

    set up config/plugins.js

--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -1,5 +1,12 @@
 module.exports = ({ env }) => ({
   "vercel-deploy": {
     enabled: true,
+    config: {
+      deployHook: process.env.VERCEL_DEPLOY_HOOK,
+      apiToken: process.env.VERCEL_DEPLOY_API_TOKEN,
+      appFilter: process.env.VERCEL_DEPLOY_APP_FILTER,
+      teamFilter: process.env.VERCEL_DEPLOY_TEAM_FILTER,
+      roles: ["strapi-super-admin", "strapi-editor", "strapi-author"],
+    },
   },
 });
